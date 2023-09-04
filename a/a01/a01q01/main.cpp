f80fcdd1f7ce17b1565d02f03576f815a565a29b
Carl Dalebout <cadalebout1@couger.ciss.edu>
2023-09-04 17:15:59 -0500

    started on a01q01

---
+++ a/a01/a01q01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    float a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7 , t_1;
+    // t0 = e * f
+    // a += b;
+    // /*
+     float tOut = a;
+     tOut += b;
+     tOut -= c*d;
+     t_1 = e*f;
+     t_1 /= g;
+     tOut += t_1;
+     a=tOut;
+    // */
+    std::cout << tOut << std::endl;
+    return 0;
+}
