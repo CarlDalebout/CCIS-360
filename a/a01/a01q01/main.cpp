de2ce58a1d1b06048cdbbedfb8b06b646179b366
Carl <cadalebout1@couger.ciss.edu>
2023-10-04 20:34:59 -0500

    commiting so i can pull

--- a/a01/a01q01/main.cpp
+++ a/a01/a01q01/main.cpp
@@ -2,18 +2,14 @@
 
 int main()
 {
-    float a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7 , t_1;
-    // t0 = e * f
-    // a += b;
-    // /*
-     float tOut = a;
-     tOut += b;
-     tOut -= c*d;
-     t_1 = e*f;
-     t_1 /= g;
-     tOut += t_1;
-     a=tOut;
-    // */
-    std::cout << tOut << std::endl;
+    int a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 1;
+    std::cin >> a >> b >> c >> d >> e >> f >> g;
+
+    // Declare your temporaty variables below
+    // For insance if you need three temporary variables below you
+    // should have "int 10, t1, t2;" below this line.
+    
+
+    std::cout << std::endl;
     return 0;
 }
